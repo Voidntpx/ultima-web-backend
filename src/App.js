ed364c6352945bfa96ddb17f72b0befe3d685740
Nattapat Sittichai <73817577+Voidntpx@users.noreply.github.com>
2023-05-22 18:46:14 +0700

    login

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import CosmeticListing from './CosmeticListing';
 import CosmeticCreate from './CosmeticCreate';
 import CosmeticDetail from './CosmeticDetail';
 import CosmeticEdit from './CosmeticEdit';
+import SignIn from './Login';
 function App() {
   return (
     <div className="App">
-      <h1>Ultima Backdoor</h1>
+      {/* <h1>Ultima Back Office</h1> */}
 
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<CosmeticListing/>}></Route>
+          <Route path='/' element={<SignIn/>}></Route>
+          <Route path='/cosmetic' element={<CosmeticListing/>}></Route>
           <Route path='/cosmetic/create' element={<CosmeticCreate />}></Route>
           <Route path='/cosmetic/detail/:cosid' element={<CosmeticDetail />}></Route>
           <Route path='/cosmetic/edit/:cosid' element={<CosmeticEdit />}></Route>
